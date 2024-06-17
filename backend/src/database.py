a497baec56677322a9e63524426be22a77d87cd3
DO-NOT-TOUCH <z5360742@ad.unsw.edu.au>
2024-06-17 16:43:50 +1000

    base database code

---
+++ backend/src/database.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+# install pymongo
+# python3 -m pip install "pymongo[srv]"
+
+# to run: python3 -m backend.src.database
+
+client = MongoClient('mongodb+srv://comp3900:wowilovecompsci123@comp3900-database.dkmw7l9.mongodb.net/?retryWrites=true&w=majority&appName=COMP3900-Database')
+
+db = client.test_database
+
+def add_user(username, password):
+    db.users.insert_one({ 'username': username, 'password': password })
+
+if __name__ == '__main__':
+    add_user('testusr', 'testpwd')
