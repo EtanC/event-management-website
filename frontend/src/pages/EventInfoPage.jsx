e6633b505baf857046a2a253545f0c30d8b37976
loeshat <90491848+loeshat@users.noreply.github.com>
2024-06-17 16:59:27 +1000

    fontend static screen pretty much done, all information is hardcoded in

--- frontend/src/pages/EventInfoPage.jsx
+++ frontend/src/pages/EventInfoPage.jsx
@@ -1,8 +1,9 @@
 // styles
-import '../styles/EventInfo.css';
+import '../styles/EventInfoPage.css';
 
 // components
 import Navbar from '../components/Navbar'
+import { useNavigate } from 'react-router-dom'
 
 import {
   Box,
@@ -15,40 +16,50 @@ import {
   Typography
 } from '@mui/material';
 import { LocationOn, CalendarToday } from '@mui/icons-material';
-import { Facebook, WhatsApp, LinkedIn, Twitter } from '@mui/icons-material';
+import { Facebook, WhatsApp, LinkedIn, Twitter, ArrowBackIosNew } from '@mui/icons-material';
 
 function EventInfoPage() {
+  const navigate = useNavigate();
+
   return (
     <>
       <Navbar />
       <Container maxWidth="lg" sx={{ marginTop: '20px' }}>
         <Card>
           <CardContent>
+            <Box sx={{ paddingLeft: '50px', paddingTop: '20px' }}>
+              <Button
+                variant='contained'
+                sx={{ textTransform: 'none', color: 'primary' }}
+                onClick={() => { navigate('/') }}
+              >
+                <ArrowBackIosNew fontSize='small' sx={{ marginRight: '5px' }} />
+                Back
+              </Button>
+            </Box>
             <Box className="header-container">
-              <Box className="header-container2" sx={{ width: '65%' }}>
-                <Typography variant="h4" component="h1" gutterBottom>
+              <Box className="header-container2" sx={{ width: '55%', justifyContent: 'space-between' }}>
+                <Typography variant="h4">
                   7th International Conference on Microbiome Engineering
                 </Typography>
                 <Typography variant="body1" color="textSecondary" paragraph>
                   This conference brings together scientists across the quickly growing field of microbiome engineering. The conference integrates synthetic biology, systems biology, microbial ecology, and bioinformatics across a range of application spaces.
                 </Typography>
               </Box>
-              <Card sx={{ padding: '20px' }}>
+              <Card sx={{ padding: '20px', width: '35%' }}>
                 <CardContent>
                   <Box className="header-container2">
                     <Grid item xs={12} md={6}>
-                      <Typography variant="h6" component="h2" gutterBottom>
+                      <Typography className="header-font">
                         Date & time
                       </Typography>
-                      <Box display="flex" alignItems="center">
-                        <CalendarToday sx={{ marginRight: '8px' }} />
-                        <Typography variant="body1">Nov 12, 2024 - Nov 14, 2024</Typography>
-                      </Box>
+                      <Typography variant="body2">Nov 12, 2024 - Nov 14, 2024</Typography>
+                      <Typography variant="body2">Add to calendar</Typography>
                     </Grid>
-                    <Button variant="contained" color="primary" sx={{ marginBottom: '20px' }}>
+                    <Button variant="contained" color="primary" sx={{ textTransform: 'none' }}>
                       Book now
                     </Button>
-                    <Button variant="outlined" sx={{ marginBottom: '20px', marginLeft: '10px' }}>
+                    <Button variant="outlined" sx={{ textTransform: 'none' }}>
                       Program promoter
                     </Button>
                   </Box>
@@ -58,71 +69,77 @@ function EventInfoPage() {
           </CardContent>
         </Card>
         <Box className="header-container">
-          <Box className="header-container2" sx={{ width: '65%' }}>
-            <Typography variant="h6" component="h2" gutterBottom>
-              Description
-            </Typography>
-            <Typography variant="body1" color="textSecondary" paragraph>
-              This conference brings together scientists across the quickly growing field of microbiome engineering. The conference integrates synthetic biology, systems biology, microbial ecology, and bioinformatics across a range of application spaces.
-            </Typography>
-            <Typography variant="body1" color="textSecondary" paragraph>
-              SESSION TOPICS:
-              <ul>
-                <li>Engineering Animal Microbiome Systems</li>
-                <li>Engineering Soil Microbiome Systems</li>
-                <li>Microbiome Engineering Tool Development</li>
-                <li>Microbiome and Probiotic Engineering for Human Health</li>
-                <li>Microbiome and Probiotic Engineering for Non-biomedical Applications</li>
-                <li>Underlying Science of Microbial Dynamics in Microbiomes</li>
-              </ul>
-            </Typography>
-            <Typography variant="h6" component="h2" gutterBottom>
-              Submission Deadline
-            </Typography>
-            <Typography variant="body1" color="textSecondary" paragraph>
-              Submission Date: Jul 22, 2024
-            </Typography>
-          </Box>
-          <Box className="header-container2">
-            <Grid container spacing={2} sx={{ marginBottom: '20px' }}>
-              <Grid item xs={12} md={6}>
-                <Typography variant="h6" component="h2" gutterBottom>
-                  Event location
-                </Typography>
-                <Box display="flex" alignItems="center">
-                  <LocationOn sx={{ marginRight: '8px' }} />
-                  <Typography variant="body1">BOSTON, MA</Typography>
-                </Box>
-              </Grid>
-            </Grid>
-            <Typography variant="h6" component="h2" gutterBottom>
-              Tags
-            </Typography>
-            <Box display="flex" flexWrap="wrap">
-              {['Indonesia event', 'Jaskaran event', 'UI', 'Seminar'].map((tag) => (
-                <Box
-                  key={tag}
-                  sx={{
-                    background: '#e0e0e0',
-                    borderRadius: '16px',
-                    padding: '5px 10px',
-                    margin: '5px',
-                  }}
-                >
-                  <Typography variant="body2">{tag}</Typography>
-                </Box>
-              ))}
-            </Box>
+          <Box className="header-container2" sx={{ width: '60%' }}>
 
-            <Typography variant="h6" component="h2" gutterBottom sx={{ marginTop: '20px' }}>
-              Share with friends
-            </Typography>
-            <Box display="flex">
-              {[Facebook, WhatsApp, LinkedIn, Twitter].map((Icon, index) => (
-                <IconButton key={index} color="primary">
-                  <Icon />
-                </IconButton>
-              ))}
+            {/* Description */}
+            <Box>
+              <Typography className="header-font">Description</Typography>
+              <Typography variant="body1" color="textSecondary" paragraph>
+                This conference brings together scientists across the quickly growing field of microbiome engineering. The conference integrates synthetic biology, systems biology, microbial ecology, and bioinformatics across a range of application spaces.
+              </Typography>
+              <Typography variant="body1" color="textSecondary" paragraph>
+                SESSION TOPICS:
+                <ul>
+                  <li>Engineering Animal Microbiome Systems</li>
+                  <li>Engineering Soil Microbiome Systems</li>
+                  <li>Microbiome Engineering Tool Development</li>
+                  <li>Microbiome and Probiotic Engineering for Human Health</li>
+                  <li>Microbiome and Probiotic Engineering for Non-biomedical Applications</li>
+                  <li>Underlying Science of Microbial Dynamics in Microbiomes</li>
+                </ul>
+              </Typography>
+            </Box>
+            {/* Dates */}
+            <Box>
+              <Typography className="header-font">Dates</Typography>
+              <Typography variant="body1" color="textSecondary" paragraph>Start Date: <span className='highlight-font'>Nov 12, 2024</span></Typography>
+              <Typography variant="body1" color="textSecondary" paragraph>End Date: <span className='highlight-font'>Nov 14, 2024</span></Typography>
+            </Box>
+            {/* Submission Deadline */}
+            <Box>
+              <Typography className="header-font">Submission Deadline</Typography>
+              <Typography variant="body1" color="textSecondary" paragraph>Submission Date: <span className='highlight-font'>Jul 22, 2024</span></Typography>
+            </Box>
+          </Box>
+
+          <Box className="header-container2" sx={{ width: '30%' }}>
+            <Box>
+              <Typography className="header-font">Event location</Typography>
+              <Box display="flex" alignItems="center">
+                <LocationOn sx={{ marginRight: '8px' }} />
+                <Typography variant="body1">BOSTON, MA</Typography>
+              </Box>
+            </Box>
+            <Box>
+              <Typography className="header-font">Tags</Typography>
+              <Box display="flex" flexWrap="wrap">
+                {['Indonesia event', 'Jaskaran event', 'UI', 'Seminar'].map((tag) => (
+                  <Box
+                    key={tag}
+                    sx={{
+                      background: '#e0e0e0',
+                      borderRadius: '16px',
+                      padding: '5px 10px',
+                      margin: '5px',
+                    }}
+                  >
+                    <Typography variant="body2">{tag}</Typography>
+                  </Box>
+                ))}
+
+              </Box>
+            </Box>
+            <Box>
+              <Typography className="header-font">
+                Share with friends
+              </Typography>
+              <Box display="flex">
+                {[Facebook, WhatsApp, LinkedIn, Twitter].map((Icon, index) => (
+                  <IconButton key={index} color="primary">
+                    <Icon />
+                  </IconButton>
+                ))}
+              </Box>
             </Box>
           </Box>
         </Box>
