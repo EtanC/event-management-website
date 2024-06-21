e23e573ce7c1de7a5cc581720f4c947b74894cf2
Lawrence Chau <lawrence.chau77@gmail.com>
2024-06-21 23:52:06 +0700

    Added storing webcrawled data into mongoDB

--- backend/easychair_scraper/easychair_scraper/pipelines.py
+++ backend/easychair_scraper/easychair_scraper/pipelines.py
@@ -1,3 +1,6 @@
+from pymongo import MongoClient
+from scrapy.exceptions import DropItem
+import logging
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -8,6 +11,24 @@
 from itemadapter import ItemAdapter
 
 
-class EasychairScraperPipeline:
+class EasychairScraperPipeline(object):
+
+    def __init__(self):
+        client = MongoClient('mongodb+srv://comp3900:wowilovecompsci123@comp3900-database.dkmw7l9.mongodb.net/?retryWrites=true&w=majority&appName=COMP3900-Database')
+        db = client.test_database
+        self.db = db
+
     def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            if self.db.events.find_one({"name": item.get("name")}) is None:
+                self.db.events.insert_one(dict(item))
+                logging.debug("Event added to MongoDB database!", extra={'spider': spider})
+            else:
+                logging.debug("Duplicate item found: {0}".format(item.get("name")), extra={'spider': spider})
+                raise DropItem("Duplicate item found: {0}".format(item.get("name")))
         return item
