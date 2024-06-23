99ff386e9b4d94829c4c5ede085a4676d8d58b33
Braian <braian.chen@student.unsw.edu.au>
2024-06-23 23:59:17 +1000

    Updated swagger documentation, added a really sketchy temporary backend api for crawling and retrieving events for thursday, after which we can come up with a better solution

--- backend/src/server.py
+++ backend/src/server.py
@@ -3,10 +3,14 @@ from flasgger import Swagger, swag_from
 from backend.swagger_doc.auth_register import auth_register_spec
 from backend.swagger_doc.auth_login import auth_login_spec
 from backend.swagger_doc.auth_logout import auth_logout_spec
+from backend.swagger_doc.events_crawl import events_crawl_spec
+from backend.swagger_doc.events_clear import events_clear_spec
+from backend.swagger_doc.events_get_all import events_get_all_spec
 from backend.src.error import AccessError, InputError
 import json
 from werkzeug.exceptions import HTTPException
 from backend.src.auth import auth_login, auth_register, auth_logout
+from backend.src.events import events_crawl, events_clear, events_get_all
 from flask_cors import CORS
 from backend.src.config import config
 
@@ -43,6 +47,20 @@ def auth_logout_route():
     body = request.get_json()
     return json.dumps(auth_logout(body['token']))
 
+@app.post('/events/crawl')
+@swag_from(events_crawl_spec)
+def events_crawl_route():
+    return json.dumps(events_crawl())
+
+@app.get('/events/get/all')
+@swag_from(events_get_all_spec)
+def events_get_all_route():
+    return json.dumps(events_get_all())
+
+@app.delete('/events/clear')
+@swag_from(events_clear_spec)
+def events_clear_route():
+    return json.dumps(events_clear())
 
 if __name__ == '__main__':
     app.run(port=config['BACKEND_PORT'],debug=True)
--- backend/swagger_doc/auth_logout.py
+++ backend/swagger_doc/auth_logout.py
@@ -1,6 +1,7 @@
 from backend.swagger_doc.definitions_auth import definitions_auth
 
 auth_logout_spec = {
+    'tags': ['Auth'],
     'parameters': [
         {
             '$ref': '#/definitions/parameters/token'
