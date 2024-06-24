62b2a933799b8857e9c4fbbb23e04feab05d7818
ethan chen <ethanchen@Ethans-Macbook.local>
2024-06-24 13:57:11 +1000

    connected login backend api with frontend, added implementations to keep track of users, note that further implementation of log out is needed to fully make this work

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
-/* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react';
+import React from 'react';
+import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import axios from 'axios';
 import {
   TextField,
@@ -18,7 +19,6 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import background from './LHSBackground.png';
 
 const LoginPage = () => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
@@ -35,9 +35,21 @@ const LoginPage = () => {
                 username: email,
                 password,
             });
-            console.log('Login successful:', response.data);
-            // Use local storage for now
-            localStorage.setItem('token', response.data.token);
+            const token = response.data.token;
+            const decodedToken = jwtDecode(token);
+            const sessionEndTime = decodedToken.session_end_time;
+
+            localStorage.setItem('token', token);
+            localStorage.setItem('session_end_time', sessionEndTime);
+
+            // Set timeout to log out the user when the session expires
+            const sessionExpiryTime = new Date(sessionEndTime).getTime() - new Date().getTime();
+            setTimeout(() => {
+                localStorage.removeItem('token');
+                localStorage.removeItem('session_end_time');
+                navigate('/login');
+            }, sessionExpiryTime);
+
             await sleep(2000);
             navigate('/'); // navigate to home
         } catch (error) {
@@ -46,131 +58,130 @@ const LoginPage = () => {
         } finally {
             setIsLoading(false);
         }
-    }; 
+    };
 
     return (
         <Grid container sx={{ height: '100vh' }}>
-        {/* Left Side */}
-        <Grid 
-        item 
-        xs={12} 
-        md={4}
-        sx={{
-            backgroundColor: '#1E4830',
-            color: '#FFFFFF',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 4,
-            backgroundImage: `url(${background})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-        }}
-        >
-            <Typography variant="h4" sx={{ marginBottom: 2, fontWeight: 'bold', fontSize: '1.8em' }}>Don't have an account?</Typography>
-            <Typography variant="body1" sx={{ marginBottom: 2 }}>Register with us today!</Typography>
-            <Button 
-                variant="contained" 
-                sx={{ 
-                    fontWeight: 'bold', 
-                    borderRadius: '15px', 
-                    mt: 2, 
-                    backgroundColor: '#91b748', 
-                    color: '#FFFFF', 
-                    '&:hover': { backgroundColor: '#1E4830' } 
-                }} 
-                onClick={() => navigate('/register')}
-                >
-                Sign up
-            </Button>
-        </Grid>
-
-        {/* Right Side */}
-        <Grid item xs={12} md={8} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 4 }}>
-            <Box elevation={6} sx={{ padding: 4, width: '100%', maxWidth: 600 }}>
-            <Typography variant="h4" align="center" gutterBottom sx={{ color: '#1E4830', fontWeight: 'bold'}}>
-                Login To Your Account
-            </Typography>
-            <Typography variant="body2" align="center" sx={{ color: '#1E4830'}}>
-                Login Using Social Networks
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 2 }}>
-                <IconButton>
-                <InstagramIcon />
-                </IconButton>
-                <IconButton>
-                <LinkedInIcon />
-                </IconButton>
-                <IconButton>
-                <FacebookIcon />
-                </IconButton>
-            </Box>
-            <Divider>OR</Divider>
-            {isLoading ? (
-                <Typography align="center" sx={{ mt: 2 }}>Loading...</Typography>
-                ) : (
-            <Box
-              component="form"
-              onSubmit={handleLogin}
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                mt: 2,
-              }}
-            >
-              <TextField
-                label="Email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                fullWidth
-                InputProps={{
-                  sx: {
-                    borderRadius: '20px',
-                  },
-                }}
-              />
-              <TextField
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                fullWidth
-                InputProps={{
-                  sx: {
-                    borderRadius: '20px',
-                  },
-                }}
-              />
-              {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
-              <Button type="submit" variant="contained" color="primary" fullWidth sx={{ fontWeight: 'bold', borderRadius: '20px' }}>
-                Login
-              </Button>
-              <Button
-                // component={Link}
-                to="/forgot-password"
-                variant="text"
-                fullWidth
+            {/* Left Side */}
+            <Grid 
+                item 
+                xs={12} 
+                md={4}
                 sx={{
-                  borderRadius: '20px',
-                  '&:hover': {
-                    backgroundColor: 'transparent',
-                  },
+                    backgroundColor: '#1E4830',
+                    color: '#FFFFFF',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 4,
+                    backgroundImage: `url(${background})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
                 }}
-              >
-                Forgot password?
-              </Button>
-            </Box>
-          )}
-        </Box>
-      </Grid>
-    </Grid>
-  );
+            >
+                <Typography variant="h4" sx={{ marginBottom: 2, fontWeight: 'bold', fontSize: '1.8em' }}>Don't have an account?</Typography>
+                <Typography variant="body1" sx={{ marginBottom: 2 }}>Register with us today!</Typography>
+                <Button 
+                    variant="contained" 
+                    sx={{ 
+                        fontWeight: 'bold', 
+                        borderRadius: '15px', 
+                        mt: 2, 
+                        backgroundColor: '#91b748', 
+                        color: '#FFFFF', 
+                        '&:hover': { backgroundColor: '#1E4830' } 
+                    }} 
+                    onClick={() => navigate('/register')}
+                >
+                    Sign up
+                </Button>
+            </Grid>
+
+            {/* Right Side */}
+            <Grid item xs={12} md={8} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 4 }}>
+                <Box elevation={6} sx={{ padding: 4, width: '100%', maxWidth: 600 }}>
+                    <Typography variant="h4" align="center" gutterBottom sx={{ color: '#1E4830', fontWeight: 'bold'}}>
+                        Login To Your Account
+                    </Typography>
+                    <Typography variant="body2" align="center" sx={{ color: '#1E4830'}}>
+                        Login Using Social Networks
+                    </Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 2 }}>
+                        <IconButton>
+                            <InstagramIcon />
+                        </IconButton>
+                        <IconButton>
+                            <LinkedInIcon />
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon />
+                        </IconButton>
+                    </Box>
+                    <Divider>OR</Divider>
+                    {isLoading ? (
+                        <Typography align="center" sx={{ mt: 2 }}>Loading...</Typography>
+                    ) : (
+                        <Box
+                            component="form"
+                            onSubmit={handleLogin}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: 2,
+                                mt: 2,
+                            }}
+                        >
+                            <TextField
+                                label="Email"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                fullWidth
+                                InputProps={{
+                                    sx: {
+                                        borderRadius: '20px',
+                                    },
+                                }}
+                            />
+                            <TextField
+                                label="Password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                fullWidth
+                                InputProps={{
+                                    sx: {
+                                        borderRadius: '20px',
+                                    },
+                                }}
+                            />
+                            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+                            <Button type="submit" variant="contained" color="primary" fullWidth sx={{ fontWeight: 'bold', borderRadius: '20px' }}>
+                                Login
+                            </Button>
+                            <Button
+                                // component={Link}
+                                to="/forgot-password"
+                                variant="text"
+                                fullWidth
+                                sx={{
+                                    borderRadius: '20px',
+                                    '&:hover': {
+                                        backgroundColor: 'transparent',
+                                    },
+                                }}
+                            >
+                                Forgot password?
+                            </Button>
+                        </Box>
+                    )}
+                </Box>
+            </Grid>
+        </Grid>
+    );
 };
 
-
-export default LoginPage; 
+export default LoginPage;
