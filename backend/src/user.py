6f08a17558dd736315632ffa1962782e7a71e1d7
Braian Chen <braian.chen@student.unsw.edu.au>
2024-07-02 16:11:59 +1000

    Fixed /auth/login to take email instead of username, finished swagger for /user/register/<event_id> and /user/events and updated server.py for these 2 functions

--- backend/src/user.py
+++ backend/src/user.py
@@ -1,8 +1,27 @@
 from backend.src.database import db
 from bson import ObjectId
+import jwt
+from backend.src.config import config
+from backend.src.error import AccessError, InputError
+from bson import ObjectId
 
-def user_register_event(user_id, event_id):
-    db.users.update_one(
+def decode_token(token):
+    try:
+        data = jwt.decode(token, config['SECRET'], algorithms=['HS256'])
+    except (jwt.InvalidSignatureError, jwt.DecodeError) as e:
+        raise AccessError('Invalid token')
+    if db.active_sessions.find_one({ '_id': ObjectId(data['session_id'])}) is None:
+        raise AccessError('Expired token')
+    if db.users.find_one({ '_id': ObjectId(data['user_id'])}) is None:
+        raise AccessError('Invalid user')
+    return data['user_id']
+
+def user_exists(user_id):
+    return db.users.find_one({ '_id': ObjectId(user_id) }) is not None
+
+def user_register_event(token, event_id):
+    user_id = decode_token(token)
+    result = db.users.update_one(
         { '_id': ObjectId(user_id) },
         {
             '$addToSet': {
@@ -10,11 +29,16 @@ def user_register_event(user_id, event_id):
             }
         }
     )
+    if result.modified_count == 0:
+        raise InputError('Already registered to event')
     return {}
 
-def user_events(user_id):
+def user_events(token):
     # Get the user document
-    user = db.users.find_one({ '_id': user_id })
+    user_id = decode_token(token)
+    user = db.users.find_one({ '_id': ObjectId(user_id) })
     # 1. Get user's list of "registered_events" event_ids
     # 2. Search for events in events collection that match these ids
-    return db.events.find({ '_id': { '$in': user['registered_events'] }})
+    return {
+        'events': list(db.events.find({ '_id': { '$in': user['registered_events'] }})),
+    }
