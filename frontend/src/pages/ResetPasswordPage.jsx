341c2cbab74372e72d941120481017b70e4f27aa
Lawrence <lawrence.chau77@gmail.com>
2024-07-31 20:58:24 +1000

    Getting rid of navbar on password reset pages + logo navigates to home

--- frontend/src/pages/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage.jsx
@@ -34,6 +34,7 @@ const ResetPasswordPage = () => {
         setConfirmPassword('')
         setSuccessMessage("Password Reset Successfully")
         setErrorMessage('')
+        navigate('/')
       }
       else {
         console.log(response.data)
@@ -79,7 +80,7 @@ const ResetPasswordPage = () => {
             {/* Right Side */}
             <Grid item xs={12} md={8} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 4, position: 'relative' }}>
                 <Box elevation={6} sx={{ padding: 4, width: '100%', maxWidth: 600 }}>
-                    <Logo />
+                    <Logo navigate={navigate} />
                     <Typography variant="h4" align="center" gutterBottom sx={{ color: '#1E4830', fontWeight: 'bold' }}>
                         Reset Password
                     </Typography>
